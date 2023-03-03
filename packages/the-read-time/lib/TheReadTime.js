4a632e10d98869f8f1c0db85249628507f27cac9
yuhongda <silverage.y@gmail.com>
2023-03-03 15:35:56 +0800

    build: 0.0.1

--- packages/the-read-time/lib/TheReadTime.js
+++ packages/the-read-time/lib/TheReadTime.js
@@ -10,14 +10,18 @@ var _extractTextFromReactNode = _interopRequireDefault(require("./extractTextFro
 var _misc = require("./misc");
 var _convertTimeToEmoji = _interopRequireDefault(require("./convertTimeToEmoji"));
 var _convertNumberToTime = _interopRequireDefault(require("./convertNumberToTime"));
+var _excluded = ["steps", "speed", "time", "displayRender"];
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
 var TheReadTime = function TheReadTime(props) {
   var _props$steps = props.steps,
     steps = _props$steps === void 0 ? _misc.DEFAULT_STEPS : _props$steps,
     _props$speed = props.speed,
     speed = _props$speed === void 0 ? _misc.DEFAULT_READ_SPEED : _props$speed,
     time = props.time,
-    displayRender = props.displayRender;
+    displayRender = props.displayRender,
+    rest = _objectWithoutProperties(props, _excluded);
   var totalTime;
   if (time) {
     totalTime = time;
@@ -31,6 +35,6 @@ var TheReadTime = function TheReadTime(props) {
     return b.minutes - a.minutes;
   });
   var emoji = (0, _convertTimeToEmoji["default"])(processSteps, totalTime);
-  return /*#__PURE__*/_react["default"].createElement("div", null, displayRender ? displayRender(emoji, totalTime) : /*#__PURE__*/_react["default"].createElement("div", null, emoji.flat().join(''), totalTimeString), props.children);
+  return /*#__PURE__*/_react["default"].createElement("div", rest, displayRender ? displayRender(emoji, totalTime) : /*#__PURE__*/_react["default"].createElement("div", null, emoji.flat().join(''), totalTimeString), props.children);
 };
 exports["default"] = exports.TheReadTime = TheReadTime;
--- packages/the-read-time/src/index.ts
+++ packages/the-read-time/src/index.ts
@@ -1,6 +1,16 @@
 import TheReadTime from './TheReadTime'
 import type { TheReadTimeProps, Step } from './types'
+import wordCount from './wordCount'
+import extractTextFromReactNode from './extractTextFromReactNode'
+import convertTimeToEmoji from './convertTimeToEmoji'
+import convertNumberToTime from './convertNumberToTime'
 
 export type { TheReadTimeProps, Step }
-export { TheReadTime }
-export default { TheReadTime }
+export { TheReadTime, wordCount, extractTextFromReactNode, convertTimeToEmoji, convertNumberToTime }
+export default {
+  TheReadTime,
+  wordCount,
+  extractTextFromReactNode,
+  convertTimeToEmoji,
+  convertNumberToTime
+}
